62c8599b7e14b1786de55ff36503ad4c39e722e6
Malyshko Serhii <malyshkosergiy@gmail.com>
2021-01-12 23:15:39 +0200

    redux counter has been completed with combineReducers()

---
+++ src/redux/counter/counter-reducer.js
@@ -0,0 +1,24 @@
+import { combineReducers } from 'redux';
+import * as actionTypes from './counter-types';
+
+const valueReducer = (state = 10, { type, payload }) => {
+  switch (type) {
+    case actionTypes.INCREMENT:
+      return state + payload;
+
+    case actionTypes.DECREMENT:
+      return state - payload;
+
+    default:
+      return state;
+  }
+};
+
+const stepReducer = (state = 5, action) => state;
+
+const counterReducer = combineReducers({
+  value: valueReducer,
+  step: stepReducer,
+});
+
+export default counterReducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,30 +1,6 @@
 import { createStore, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-
-const counterInitialState = {
-  value: 10,
-  step: 50,
-};
-
-const valueReducer = (state = 10, { type, payload }) => {
-  switch (type) {
-    case 'counter/Increment':
-      return state + payload;
-
-    case 'counter/Decrement':
-      return state - payload;
-
-    default:
-      return state;
-  }
-};
-
-const stepReducer = (state = 5, action) => state;
-
-const counterReducer = combineReducers({
-  value: valueReducer,
-  step: stepReducer,
-});
+import counterReducer from './counter/counter-reducer';
 
 const rootReducer = combineReducers({ counter: counterReducer });
 
